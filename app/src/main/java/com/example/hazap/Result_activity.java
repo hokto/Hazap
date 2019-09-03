6636ed92dfd85b1ec7af21ee3b015ef6dda04b65
Shougo Hiroike <piroty20010803@yahoo.co.jp>
2019-09-03 22:44:24 +0900

    コードの整理

--- app/src/main/java/com/example/hazap/Result_activity.java
+++ app/src/main/java/com/example/hazap/Result_activity.java
@@ -7,23 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 /*
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 4e8cead12b5fe8d3f1c5ece3214a29c8c9849113
 */
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Result_activity extends Activity   {
     @Override
