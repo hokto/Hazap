a55641e9eb17e6de54da89d8769756fbfacc8c3b
Shougo Hiroike <piroty20010803@yahoo.co.jp>
2019-09-10 16:25:58 +0900

    管理者画面に戻るボタンの追加、及び選択項目のテキストサイズを調整

--- app/src/main/java/com/example/hazap/Organizer.java
+++ app/src/main/java/com/example/hazap/Organizer.java
@@ -1,36 +1,26 @@
 package com.example.hazap;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.location.Location;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.view.GravityCompat;
-import android.util.TypedValue;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.ExecutionException;
 
 import jp.co.yahoo.android.maps.GeoPoint;
-import jp.co.yahoo.android.maps.MapActivity;
 import jp.co.yahoo.android.maps.MapView;
 import jp.co.yahoo.android.maps.MyLocationOverlay;
 import jp.co.yahoo.android.maps.PinOverlay;
@@ -57,7 +47,7 @@ public class Organizer extends Activity {
         disasterSpinner=findViewById(R.id.disasterspinner);//災害の種類を選ばせる処理の設定
         final RelativeLayout relativeLayout=findViewById(R.id.relativeLayout);
         nextSpinner=new Spinner(this);
-        ArrayAdapter<String> disasterAdapter=new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,disasterItems);
+        ArrayAdapter<String> disasterAdapter=new ArrayAdapter<>(this,R.layout.spinner_item,disasterItems);
         disasterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         disasterSpinner.setAdapter(disasterAdapter);
         disasterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){ //災害の種類を選ぶところが押された場合
@@ -80,6 +70,15 @@ public class Organizer extends Activity {
                 public void onNothingSelected(AdapterView<?> parent){
                     //何も選択されていないときは特に何もしない
                 }
+        });
+        Button back_button = findViewById(R.id.back_btn);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent home_intent = new Intent(getApplication(),MainActivity.class);
+
+                startActivity(home_intent);
+            }
         });
         Button startbtn=new Button(this);//避難開始用のボタン
         startbtn.setText("訓練開始");
@@ -210,7 +209,7 @@ public class Organizer extends Activity {
     }
     private void setSpinnerItems(Spinner spinner,String[] items,RelativeLayout relativeLayout,MainActivity playDisplay){ //各災害における選択肢の設定
         relativeLayout.removeView(spinner);//前に設定されていたものを取り除く
-        ArrayAdapter nextAdapter=new ArrayAdapter(this,android.R.layout.simple_spinner_item,items);
+        ArrayAdapter nextAdapter=new ArrayAdapter(this,R.layout.spinner_item,items);
         nextAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(nextAdapter);
         RelativeLayout.LayoutParams nextParams=new RelativeLayout.LayoutParams(820* MainActivity.DisplayWidth /1080,200* MainActivity.DisplayHeight /1794);
