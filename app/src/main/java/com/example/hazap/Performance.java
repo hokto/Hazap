31baba88a687f69070b66ff51a3635e45e3e0d98
Shougo Hiroike <piroty20010803@yahoo.co.jp>
2019-09-13 10:00:30 +0900

    GUIを少し修正

--- app/src/main/java/com/example/hazap/Performance.java
+++ app/src/main/java/com/example/hazap/Performance.java
@@ -1,10 +1,14 @@
 package com.example.hazap;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -24,6 +28,7 @@ import java.util.List;
 public class Performance extends Activity {
     private static int aliveRate;//生存率
     private static Bitmap routeMap;//サーバから取得した避難結果の画像を格納
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -82,6 +87,10 @@ public class Performance extends Activity {
         ratetxtParam.topMargin=213*display.DisplayHeight/1216;
         relativeLayout.addView(aliveRatetxt,ratetxtParam);
         Button btn=new Button(this);//ホームに戻るボタンの設定
+        Drawable btn_color = ResourcesCompat.getDrawable(getResources(), R.drawable.button_state, null);//リソースから作成したDrawableのリソースを取得
+        btn.setBackground(btn_color);//ボタンにDrawableを適用する
+        btn.setTextColor(Color.parseColor("#FFFFFF"));//ボタンの文字の色を白に変更する
+        btn.setTextSize(TypedValue.COMPLEX_UNIT_SP,30);//ボタンの文字の大きさを調節
         btn.setText("戻る");
         btn.setTextSize(20*displayHypotenuse/baseHypotenuse);
         RelativeLayout.LayoutParams btnParam=new RelativeLayout.LayoutParams(250*display.DisplayWidth/800,100*display.DisplayHeight/1216);
